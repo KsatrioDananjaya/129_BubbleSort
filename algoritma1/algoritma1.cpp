a85f6e1912dde409b57d8ffd4c74605fddb4342e
KsatrioDananjaya <ksatrio.dana.ft20@mail.umy.ac.id>
2024-03-07 11:05:50 +0700

    input

--- algoritma1/algoritma1.cpp
+++ algoritma1/algoritma1.cpp
@@ -1,7 +1,21 @@
 #include <iostream>
+using namespace std;
 
 int a[20];
 int n;
+
+void input() {
+    while (true) {
+        cout << "Masukkan banyaknya elemen pada array : ";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "n\Array dapat mempunyai masimal 20 elemen.\n";
+        }
+    }
+}
+
 
 int main()
 {
